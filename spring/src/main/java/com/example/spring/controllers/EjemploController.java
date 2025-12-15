4d3cac6fbdd0ed3e710be6fd60e39f893cc9eb32
Alcindo One Chavarria <139635361+ByOneChav@users.noreply.github.com>
2025-12-14 22:52:23 -0300

    agregar archivos al controllers MVC y methodos (anotaciones)

--- spring/src/main/java/com/example/spring/controllers/EjemploController.java
+++ spring/src/main/java/com/example/spring/controllers/EjemploController.java
@@ -1,8 +1,12 @@
 package com.example.spring.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.spring.models.Empleado;
 
@@ -20,6 +24,16 @@ public class EjemploController {
         Empleado empleado1 = new Empleado("Onega", "Chavarria", "av Lastarria 943", "Developer", 28, 940673637, 01);
         model.addAttribute("Empleado", empleado1);
         return "detalles_info";
+    }
+
+    @ModelAttribute("Empleados")
+    public List<Empleado> ListaEmpleados(){
+        return Arrays.asList(new Empleado("Maria", "Perez", "Calle segundo n.2", "Secretaria", 21, 98788772, 02),
+          new Empleado("María", "Perez", "Calle segunda no.2", "Secretaria", 20, 123654987, 02),
+          new Empleado("Juan", "Rodriguez", "Calle primera no.1", "Gerente", 35, 123456789, 01),
+          new Empleado("Luis", "Martínez", "Calle tercera no.3", "Contador", 28, 987654321, 03),
+          new Empleado("Ana", "González", "Avenida central no.4", "Diseñadora", 30, 456789123, 04),
+          new Empleado("Pedro", "Sánchez", "Boulevard no.5", "Analista", 25, 789123456, 05));
     }
 
 }
